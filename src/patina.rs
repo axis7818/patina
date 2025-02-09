3619ca3a1966cd232b17a0210e761c3c253d189c
Cameron Taylor <axis7818@gmail.com>
2025-02-09 10:31:06 -0800

    Yaml to Toml (#3)
    * yaml to toml
    
    * update workflow
    
    * update workflow

--- src/patina.rs
+++ src/patina.rs
@@ -1,29 +1,22 @@
 use std::path::PathBuf;
 
-use yaml_rust::YamlLoader;
+use serde::{Deserialize, Serialize};
 
-use crate::patina_yaml::FromYaml;
-
-#[derive(Debug, Default, PartialEq)]
+#[derive(Debug, Default, PartialEq, Serialize, Deserialize)]
 pub struct Patina {
     pub name: String,
+
+    #[serde(default)]
     pub description: String,
+
+    #[serde(default)]
     pub files: Vec<PatinaFile>,
 }
 
-#[derive(Debug, Default, PartialEq)]
+#[derive(Debug, Default, PartialEq, Serialize, Deserialize)]
 pub struct PatinaFile {
     pub template: PathBuf,
     pub target: PathBuf,
-}
-
-#[derive(Debug)]
-#[allow(clippy::enum_variant_names)]
-pub enum ValidationError {
-    NameMissing,
-    DescriptionMissing,
-    FileTemplateMissing,
-    FileTargetMissing,
 }
 
 #[derive(Debug)]
@@ -31,26 +24,145 @@ pub enum ValidationError {
 pub enum Error {
     FileRead(std::io::Error),
     YamlParse(yaml_rust::ScanError),
-    Validation(ValidationError),
+    TomlParse(toml::de::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 impl Patina {
-    pub fn from_yaml_file(yaml_file_path: &PathBuf) -> Result<Vec<Patina>> {
-        let yaml_str = match std::fs::read_to_string(yaml_file_path) {
-            Ok(yaml_str) => yaml_str,
+    pub fn from_toml_file(toml_file_path: &PathBuf) -> Result<Patina> {
+        let toml_str = match std::fs::read_to_string(toml_file_path) {
+            Ok(toml_str) => toml_str,
             Err(e) => return Err(Error::FileRead(e)),
         };
 
-        let yaml = match YamlLoader::load_from_str(&yaml_str) {
-            Ok(yaml) => yaml,
-            Err(e) => return Err(Error::YamlParse(e)),
-        };
-
-        match yaml.iter().map(Patina::from_yaml).collect() {
-            Ok(patina) => Ok(patina),
-            Err(e) => Err(Error::Validation(e)),
+        match toml::from_str(&toml_str) {
+            Ok(toml) => Ok(toml),
+            Err(e) => Err(Error::TomlParse(e)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_patina_deserialize() {
+        let patina = r#"
+            name = "simple-patina"
+            description = "This is a simple Patina example"
+
+            [vars]
+            name = "Patina"
+
+            [[files]]
+            template = "./templates/hello.txt"
+            target = "./output/hello.txt"
+        "#;
+
+        let patina = toml::from_str::<Patina>(patina);
+        assert!(patina.is_ok());
+        let patina = patina.unwrap();
+
+        assert_eq!(patina.name, "simple-patina");
+        assert_eq!(patina.description, "This is a simple Patina example");
+
+        let patina_files = patina.files;
+        assert_eq!(patina_files.len(), 1);
+        let patina_file = &patina_files[0];
+        assert_eq!(patina_file.template, PathBuf::from("./templates/hello.txt"));
+        assert_eq!(patina_file.target, PathBuf::from("./output/hello.txt"));
+    }
+
+    #[test]
+    fn test_patina_deserialize_name_missing() {
+        let patina = r#"
+            description = "This is a simple Patina example"
+
+            [vars]
+            name = "Patina"
+
+            [[files]]
+            template = "./templates/hello.txt"
+            target = "./output/hello.txt"
+        "#;
+
+        let patina = toml::from_str::<Patina>(patina);
+        assert!(patina.is_err());
+        assert_eq!(patina.unwrap_err().message(), "missing field `name`");
+    }
+
+    #[test]
+    fn test_patina_deserialize_default_description() {
+        let patina = r#"
+            name = "simple-patina"
+
+            [vars]
+            name = "Patina"
+
+            [[files]]
+            template = "./templates/hello.txt"
+            target = "./output/hello.txt"
+        "#;
+
+        let patina = toml::from_str::<Patina>(patina);
+        assert!(patina.is_ok());
+        let patina = patina.unwrap();
+
+        assert!(patina.description.is_empty());
+    }
+
+    #[test]
+    fn test_patina_deserialize_default_files() {
+        let patina = r#"
+            name = "simple-patina"
+            description = "This is a simple Patina example"
+
+            [vars]
+            name = "Patina"
+        "#;
+
+        let patina = toml::from_str::<Patina>(patina);
+        assert!(patina.is_ok());
+        let patina = patina.unwrap();
+
+        assert!(patina.files.is_empty());
+    }
+
+    #[test]
+    fn test_patina_file_template_missing() {
+        let patina = r#"
+            name = "simple-patina"
+            description = "This is a simple Patina example"
+
+            [vars]
+            name = "Patina"
+
+            [[files]]
+            target = "./output/hello.txt"
+        "#;
+
+        let patina = toml::from_str::<Patina>(patina);
+        assert!(patina.is_err());
+        assert_eq!(patina.unwrap_err().message(), "missing field `template`");
+    }
+
+    #[test]
+    fn test_patina_file_target_missing() {
+        let patina = r#"
+            name = "simple-patina"
+            description = "This is a simple Patina example"
+
+            [vars]
+            name = "Patina"
+
+            [[files]]
+            template = "./templates/hello.txt"
+        "#;
+
+        let patina = toml::from_str::<Patina>(patina);
+        assert!(patina.is_err());
+        assert_eq!(patina.unwrap_err().message(), "missing field `target`");
     }
 }

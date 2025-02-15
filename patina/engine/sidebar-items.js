9a8d761a561d93508dc3b3475cd70f41584b4f1c
axis7818 <axis7818@users.noreply.github.com>
2025-02-15 17:05:35 +0000

    deploy: 7fabf5afb605dc963a0d3f5859af0314e8ec05b3

--- patina/engine/sidebar-items.js
+++ patina/engine/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["apply_patina_from_file","render_patina_from_file"]};
+window.SIDEBAR_ITEMS = {"fn":["apply_patina_from_file","render_patina_from_file"],"mod":["interface"]};

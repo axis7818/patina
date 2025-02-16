c6e1dbdbd78f1360f58332749b1fb4eee4a9d2c9
Cameron Taylor <axis7818@gmail.com>
2025-02-16 08:22:00 -0800

    refs heads main

--- scripts/check_version.sh
+++ scripts/check_version.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-PREV_VERSION=$(git show main:Cargo.toml | grep '^version' | awk '{print $3}')
+PREV_VERSION=$(git show refs/heads/main:Cargo.toml | grep '^version' | awk '{print $3}')
 if [ -z "$PREV_VERSION" ]; then
-    echo "Previous version not found."
+    echo "❌ Previous version not found."
     exit 1
 fi
 echo "Previous version = ${PREV_VERSION}"
 
 CURR_VERSION=$(grep '^version' Cargo.toml | awk '{print $3}')
 if [ -z "$CURR_VERSION" ]; then
-    echo "Current version not found."
+    echo "❌ Current version not found."
     exit 1
 fi
 echo "Current version = ${CURR_VERSION}"

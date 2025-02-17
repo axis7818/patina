62f04eb246b8ecc318a447df522138e3667d026f
Cameron Taylor <axis7818@gmail.com>
2025-02-16 19:35:31 -0800

    Support home directory files (#20)
    * clean up readme
    
    * vars example
    
    * normalize paths
    
    * 0.1.6
    
    * home dir support
    
    * quit early if there are no changes

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,7 @@
-use std::path::PathBuf;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use enum_as_inner::EnumAsInner;
 
@@ -6,6 +9,9 @@ use enum_as_inner::EnumAsInner;
 #[allow(dead_code)]
 #[derive(Debug, EnumAsInner)]
 pub enum Error {
+    /// A general error with a message
+    Message(String),
+
     /// An error that occurs when a file cannot be read
     FileRead(PathBuf, std::io::Error),
 
@@ -24,3 +30,55 @@ pub enum Error {
 
 /// A Result type that uses the [`Error`] enum
 pub type Result<T> = std::result::Result<T, Error>;
+
+/// Given a path, normalize it to an absolute path with cwd (`.`), home (`~`), and environment variables resolved.
+pub fn normalize_path<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
+    let path = path.as_ref();
+
+    // resolve home dir and environment variables
+    let path = path.to_str()?;
+    let path = match shellexpand::full(path) {
+        Ok(path) => path.into_owned(),
+        Err(_) => return None,
+    };
+
+    // clean it by resolving `.` and multiple `/`s
+    let path = path_clean::clean(path);
+
+    // get the canonical path
+    match fs::canonicalize(&path) {
+        Ok(path) => Some(path),
+        Err(_) => Some(path),
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use std::path::PathBuf;
+
+    use super::normalize_path;
+
+    pub fn get_home_dir() -> String {
+        let home_dir = dirs::home_dir().unwrap();
+        let home_dir = home_dir.to_str().unwrap();
+        String::from(home_dir)
+    }
+
+    #[test]
+    fn test_normalize_path() {
+        let path = normalize_path(PathBuf::from("path/to/file.txt"));
+        assert_eq!(PathBuf::from("path/to/file.txt"), path.unwrap());
+
+        let path = normalize_path(PathBuf::from("path/to/../file.txt"));
+        assert_eq!(PathBuf::from("path/file.txt"), path.unwrap());
+
+        let path = normalize_path(PathBuf::from("path/to///file.txt"));
+        assert_eq!(PathBuf::from("path/to/file.txt"), path.unwrap());
+
+        let path = normalize_path(PathBuf::from("~/path/to/file.txt"));
+        assert_eq!(
+            PathBuf::from(format!("{}/path/to/file.txt", get_home_dir())),
+            path.unwrap()
+        );
+    }
+}
